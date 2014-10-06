07f542e8fc00be33b0ff155409c4991a3e3bb1d1
Joshua Liben <joshua2579@gmail.com>
2014-10-06 18:29:45 -0400

    Added some test cases

--- test_exercise3.py
+++ test_exercise3.py
@@ -6,7 +6,7 @@ from exercise3 import decide_rps
 
 def test_checksum():
     """
-    Inputs that are the correct format and length
+    Inputs that output the correct results
     """
     assert decide_rps("Rock", "Paper") == 2
     assert decide_rps("Scissors", "Scissors") == 0
@@ -17,4 +17,18 @@ def test_checksum():
     assert decide_rps("Paper", "Paper") == 0
     assert decide_rps("Rock", "Rock") == 0
     assert decide_rps("Scissors", "Paper") == 1
+
+def test_input():
+    """
+    Inputs that are the correct value and type
+    """
+    with pytest.raises(ValueError):
+        decide_rps("Rock","Sissors")
+        decide_rps("Roc", "Scissors")
+        decide_rps("something", "else")
+
+    with pytest.raises(TypeError):
+        decide_rps(2,"Sissors")
+        decide_rps("Roc", 2.09)
+        decide_rps(2.9876, 3.0987)
 
